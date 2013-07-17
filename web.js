ebaa094db091f5911bd71651b3c2dbc5c6ec6300
vrozkovec <vit.rozkovec@gmail.com>
2013-07-17 13:34:18 +0000

    Read index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html', function (err, data) {
+  fs.readFileSync('~/src/bitstarter/index.html', function (err, data) {
       if (err) throw err;
       var buffer = new Buffer(data, 'utf-8');
       console.log(buffer.toString('utf-8'));
